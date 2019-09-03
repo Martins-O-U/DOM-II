1cd00b56c7870fec2592c41eab0c590b9d262c99
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-03 15:41:05 +0100

    Added a double click event listener as well as prevented default on the Navigation link

--- js/index.js
+++ js/index.js
@@ -8,40 +8,52 @@ window.addEventListener('load', (event) => {
 
 
  let Para = document.querySelectorAll("p");
-  Para[0].addEventListener("mouseover", function( event ) {   
+  Para[0].addEventListener("mouseover", (event) =>{   
     event.target.style.background = "orange";
   });
 
-  Para[0].addEventListener("mouseout", function( event ) {   
+  Para[0].addEventListener("mouseout", (event) => {   
     event.target.style.background = "";
   })
 
 
-  Para[1].addEventListener("mouseover", function( event ) {   
+  Para[1].addEventListener("mouseover", (event) => {   
     event.target.style.background = "orange";
   });
 
-  Para[1].addEventListener("mouseout", function( event ) {   
+  Para[1].addEventListener("mouseout", (event) => {   
     event.target.style.background = "";
   })
 
 
-  Para[2].addEventListener("mouseover", function( event ) {   
+  Para[2].addEventListener("mouseover", (event) => {   
     event.target.style.background = "orange";
   });
 
-  Para[2].addEventListener("mouseout", function( event ) {   
+  Para[2].addEventListener("mouseout", (event) => {   
     event.target.style.background = "";
   })
 
 
 let btnClick = document.querySelector("#btn1");
-btnClick.addEventListener("click", (events)=>{
+btnClick.addEventListener("dblclick", (events)=>{
    let contPara = document.querySelector('.destination p');
-   console.log(contPara);
    contPara.textContent = "Congratulations! You just won a bonus FREE WEEKEND STAY in Blinx Hotels!!!";
 
    setTimeout(function() {
     contPara.textContent = "Expedition excursion design excursion fun, clean simple organized WordPress Travel colorful webdesign. Traveler blogger website design expedition clean excursion traveling.";
   }, 1500);
 } )
+
+let navBar = document.querySelector("nav");
+navBar.addEventListener("click", (event)=>{
+window.alert("Default link has been Prevented");
+event.preventDefault();
+})
+  
+
+  const input = document.querySelector('p');
+  input.addEventListener('select', ()=>{
+    const selection = event.target.substring(event.target.selectionStart, event.target.selectionEnd);
+      window.alert(`You selected: ${selection}`);
+  });
